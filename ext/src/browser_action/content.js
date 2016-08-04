8af692e4d0f9cf9d6bc46fc4fae500143c784446
Torbjorn Tyridal <ttyridal@users.noreply.github.com>
2016-08-04 12:55:54 +0200

    Emit change event on password field after pasting password
    This is required by sites using AngularJS or others
    applying a listener approach to data collection
    
    (eg: accounts.firefox.com, protonmail.com)
    
    https://github.com/ttyridal/masterpassword-firefox/issues/19

--- ext/src/browser_action/content.js
+++ ext/src/browser_action/content.js
@@ -1,11 +1,14 @@
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.sender && msg.sender == "no.tyridal.masterpassword" && msg.password &&
-        document.activeElement && 
+        document.activeElement &&
         document.activeElement.tagName &&
         document.activeElement.tagName == "INPUT" &&
         document.activeElement.type == "password") {
         document.activeElement.value = msg.password;
         sendResponse({sender:"no.tyridal.masterpassword", pasted:true});
-    } 
+
+        document.activeElement.dispatchEvent(
+            new Event('change', {bubbles: true, cancelable: true}));
+    }
 });
 
