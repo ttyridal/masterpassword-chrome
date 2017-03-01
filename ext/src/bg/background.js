330822d83e30d1e873c7d9a73f3bbfd58f2e1103
Torbjorn Tyridal <ttyridal@users.noreply.github.com>
2017-03-01 15:09:23 +0100

    pwvault_nativebridge
    fixed id-key is a requirement

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -15,11 +15,50 @@
     You should have received a copy of the GNU General Public License
     along with the software.  If not, see <http://www.gnu.org/licenses/>.
 */
-/* global browser, console, chrome */
+/* global console, chrome */
 /* jshint esversion: 6 */
 
 (function(){
 "use strict";
+
+var port;
+function port_default_error(p)  { port = undefined; }
+function pwvault_gateway(msg) {
+    console.log("pwvault_gw:",msg.type);
+    // Keeping the port open "forever".. seems to be a bug in firefox
+    // not noting that the native-app is gone and it will spinn forever.
+    // Like this, we'll at least not trigger that until firefox closes.
+
+    if (!port) {
+        port = chrome.runtime.connectNative('no.ttyridal.pwvault_gateway');
+        port.onDisconnect.addListener(port_default_error);
+    }
+
+    return new Promise((resolv, fail) => {
+        let error;
+        let success = r => {
+            port.onMessage.removeListener(error);
+            port.onDisconnect.removeListener(success);
+            resolv(r);
+        };
+        error = p => {
+            p = p.error;
+            if (!p) p = "disconnect";
+            port = undefined;
+            fail(p);
+        };
+
+        port.onMessage.addListener(success);
+        port.onDisconnect.addListener(error);
+        try {
+            port.postMessage(msg);
+        } catch (err) {
+            port.onMessage.removeListener(error);
+            port.onDisconnect.removeListener(success);
+            fail(err);
+        }
+    });
+}
 
 var settings = {
     'defaulttype': 'l',
@@ -73,7 +112,12 @@ function store_update(d) {
                     syncset[k] = d[k];
                 break;
             case 'masterkey':
-                temp_store_masterkey(d[k]);
+                if (settings.pass_store !== 'n') {
+                    if (d.key_id || d.force_update)
+                        Promise.resolve(pwvault_gateway({'type':'pwset','name':'default', 'value': d[k]}))
+                        .catch(e => { console.error(e); });
+                } else
+                    temp_store_masterkey(d[k]);
                 break;
             default:
                 console.info("Trying to store unknown key",k);
@@ -130,7 +174,16 @@ function store_get(keys) {
         return r;
     })
     .then(r => {
-        return [r, {success: true, value: _masterkey}];
+        if (settings.pass_store !== 'n' && keys.indexOf('masterkey') !== -1) {
+            return Promise.all([r,
+                pwvault_gateway({'type':'pwget', 'name':'default'})
+                .catch(err => {
+                    console.error("pwvault_gateway failed " + err);
+                    return {success:false, reason:err};
+                })
+            ]);
+        } else
+            return [r, {success: true, value: _masterkey}];
     })
     .then(comb => {
         let [r, mk] = comb;
